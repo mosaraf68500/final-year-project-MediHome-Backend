4828b3f9815a86b197b89bc000aa9e7ee7b86747
mosaraf68500 <mosaraf.cse8.bu@gmail.com>
2026-05-20 09:17:01 +0600

    create doctor route done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { SpecialtyRouter } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { UserRouters } from "../module/user/user.route";
 
 const router =Router();
 
 
 router.use('/auth',AuthRoutes)
 router.use('/specialities',SpecialtyRouter)
+
+router.use('/doctor',UserRouters);
 
 export const IndexRoutes=router;
